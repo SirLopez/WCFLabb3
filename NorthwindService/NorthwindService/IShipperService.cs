ba9fffa80925c38dcd14382806d953bec0cf6ad1
SirLopez <daniel.lopez@live.se>
2016-02-03 13:32:37 +0100

    GetShipper & UpdateShipper working

--- NorthwindService/NorthwindService/IShipperService.cs
+++ NorthwindService/NorthwindService/IShipperService.cs
@@ -8,10 +8,24 @@ using System.Text;
 namespace NorthwindService
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IShipperService" in both code and config file together.
-    [ServiceContract]
+    [ServiceContract(Namespace = "EC.WCFLABB3")]
     public interface IShipperService
     {
         [OperationContract]
-        void DoWork();
+        Shipper GetShipperByID(int id);
+
+        [OperationContract]
+        void SaveShipper(int id, string companyName, string phone);
+    }
+
+    [DataContract]
+    public class Shipper
+    {
+        [DataMember]
+        public int ShipperID { get; set; }
+        [DataMember]
+        public string CompanyName { get; set; }
+        [DataMember]
+        public string Phone { get; set; }
     }
 }
--- NorthwindService/NorthwindService/ShipperService.svc.cs
+++ NorthwindService/NorthwindService/ShipperService.svc.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace NorthwindService
 {
@@ -11,8 +14,48 @@ namespace NorthwindService
     // NOTE: In order to launch WCF Test Client for testing this service, please select ShipperService.svc or ShipperService.svc.cs at the Solution Explorer and start debugging.
     public class ShipperService : IShipperService
     {
-        public void DoWork()
+
+        public Shipper GetShipperByID(int id)
         {
+            var theShipper = new Shipper();
+            var connectionString = ConfigurationManager.ConnectionStrings["NORTHWND"].ConnectionString;
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var sqlShipperCommand = new SqlCommand("SELECT * FROM Shippers WHERE ShipperID = @id", conn);
+                sqlShipperCommand.Parameters.AddWithValue("@id", id);
+
+                using (var reader = sqlShipperCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        theShipper.ShipperID = (int)reader["ShipperID"];
+                        theShipper.CompanyName = reader["CompanyName"].ToString();
+                        theShipper.Phone = reader["Phone"].ToString(); 
+                    }
+                }
+                conn.Close();
+            }
+            return theShipper;
+        }
+
+        public void SaveShipper(int id, string companyName, string phone)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["NORTHWND"].ConnectionString;
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                var saveCmd =
+                    new SqlCommand(
+                        "UPDATE Shippers SET CompanyName = @companyName, Phone = @phone WHERE ShipperID = @id", conn);
+                saveCmd.Parameters.AddWithValue("@id", id);
+                saveCmd.Parameters.AddWithValue("@companyName", companyName);
+                saveCmd.Parameters.AddWithValue("@phone", phone);
+
+                saveCmd.ExecuteNonQuery();
+            }
         }
     }
 }
